9720901662e845687c5cdc9b5ccd2b00184068bb
mengfantao <mengfantao@hzyushi.cn>
2018-08-08 17:59:24 +0800

    加入分享模块

---
+++ app/src/main/java/com/yushi/leke/wxapi/WXPayEntryActivity.java
@@ -0,0 +1,53 @@
+package com.yushi.leke.wxapi;
+
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.tencent.mm.sdk.modelbase.BaseReq;
+import com.tencent.mm.sdk.modelbase.BaseResp;
+import com.tencent.mm.sdk.openapi.IWXAPI;
+import com.tencent.mm.sdk.openapi.IWXAPIEventHandler;
+import com.tencent.mm.sdk.openapi.WXAPIFactory;
+
+public class WXPayEntryActivity extends Activity implements IWXAPIEventHandler {
+
+
+    private IWXAPI api;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        api = WXAPIFactory.createWXAPI(this, "wx896661328515ce2f");
+        api.handleIntent(getIntent(), this);
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        setIntent(intent);
+        api.handleIntent(intent, this);
+    }
+
+
+    @Override
+    public void onReq(BaseReq baseReq) {
+
+    }
+
+    @Override
+    public void onResp(BaseResp resp) {
+        if (resp.errCode == 0) {
+            Toast.makeText(this,"支付成功",Toast.LENGTH_SHORT).show();
+        } else {
+
+        }
+        finish();
+
+    }
+
+
+}
