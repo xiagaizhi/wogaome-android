eb6301e6047bd20261256c3ab33f0ed7efff20eb
Zhanyangyang <zhanyangyang@hzyushi.cn>
2018-08-22 19:51:47 +0800

    包名更改

--- app/src/main/java/com/yushi/leke/fragment/main/MainMenu.java
+++ app/src/main/java/com/yushi/leke/fragment/main/MainMenu.java
@@ -13,7 +13,6 @@ public enum MainMenu {
     ROAD_SHOW_HALL("路演大厅", R.drawable.tab_icon_roadshow),
     MY("我的", R.drawable.tab_icon_my);
     private String title;
-    private Class<?> clz;
     private int icon;
     MainMenu(String title,int icon) {
         this.title = title;
