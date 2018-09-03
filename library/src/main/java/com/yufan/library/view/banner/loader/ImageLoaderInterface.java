df7564145d84ce78d5d27299f3571b4683a702ea
mengfantao <mengfantao@hzyushi.cn>
2018-09-03 19:37:53 +0800

    首页banner

---
+++ library/src/main/java/com/yufan/library/view/banner/loader/ImageLoaderInterface.java
@@ -0,0 +1,14 @@
+package com.yufan.library.view.banner.loader;
+
+import android.content.Context;
+import android.view.View;
+
+import java.io.Serializable;
+
+
+public interface ImageLoaderInterface<T extends View> extends Serializable {
+
+    void displayImage(int position, Context context, Object path, T imageView);
+
+    T createImageView(Context context);
+}
