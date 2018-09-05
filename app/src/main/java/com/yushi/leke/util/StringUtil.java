733aa93bec66e28d2d51213844083f57cb453d0b
Zhanyangyang <zhanyangyang@hzyushi.cn>
2018-09-05 15:40:17 +0800

    我的钱包

--- app/src/main/java/com/yushi/leke/util/StringUtil.java
+++ app/src/main/java/com/yushi/leke/util/StringUtil.java
@@ -2,10 +2,14 @@ package com.yushi.leke.util;
 
 import android.content.ClipboardManager;
 import android.content.Context;
+import android.text.Html;
+import android.text.Spannable;
+import android.text.Spanned;
 import android.text.TextUtils;
 
 import com.yufan.library.manager.DialogManager;
 import com.yushi.leke.App;
+import com.yushi.leke.fragment.wallet.SizeLabel;
 
 /**
  * 作者：Created by zhanyangyang on 2018/9/1 11:24
@@ -31,5 +35,41 @@ public class StringUtil {
                 return phoneNumber;
             }
         }
+    }
+
+    public static String[] formatMoney(String money) {
+        String head = "0";
+        String tail = "";
+        if (!TextUtils.isEmpty(money)) {
+            String[] lkcs1 = money.split("\\.");
+            String[] lkcs2 = money.split("//.");
+
+            if (lkcs1.length > 1) {
+                tail = lkcs1[1];
+                if (TextUtils.isEmpty(tail)) {
+                    head = lkcs1[0];
+                } else {
+                    head = lkcs1[0] + ".";
+                }
+            } else {
+                if (lkcs2.length > 1) {
+                    tail = lkcs2[1];
+                    if (TextUtils.isEmpty(tail)) {
+                        head = lkcs2[0];
+                    } else {
+                        head = lkcs2[0] + ".";
+                    }
+                } else {
+                    head = money;
+                    tail = "";
+                }
+            }
+        }
+        return new String[]{head, tail};
+    }
+
+    public static Spanned getMoneySpannble(String money, Context context) {
+        String[] datas = formatMoney(money);
+        return Html.fromHtml("<b><<font color='#151515'><size>" + datas[0] + "</size></font></b><font color='#333333'><size2>" + datas[1] + "</size2></font>", null, new SizeLabel(context));
     }
 }
