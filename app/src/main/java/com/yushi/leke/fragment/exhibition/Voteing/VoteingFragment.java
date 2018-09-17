02cea8f54a58c2915bc3ca6db5906b29211bf561
Zhanyangyang <zhanyangyang@hzyushi.cn>
2018-09-17 11:19:23 +0800

    投票详情

--- app/src/main/java/com/yushi/leke/fragment/exhibition/Voteing/VoteingFragment.java
+++ app/src/main/java/com/yushi/leke/fragment/exhibition/Voteing/VoteingFragment.java
@@ -14,6 +14,7 @@ import com.yufan.library.api.BaseHttpCallBack;
 import com.yufan.library.base.BaseListFragment;
 import com.yufan.library.inject.VuClass;
 import com.yufan.library.inter.ICallBack;
+import com.yufan.library.view.recycler.PageInfo;
 import com.yushi.leke.UIHelper;
 import com.yushi.leke.YFApi;
 import com.yushi.leke.fragment.exhibition.Voteing.allproject.allprojectsFragment;
@@ -27,31 +28,70 @@ import me.drakeet.multitype.MultiTypeAdapter;
 public class VoteingFragment extends BaseListFragment<VoteingContract.IView> implements VoteingContract.Presenter {
     private MultiTypeAdapter adapter;
     private Voteinginfolist doendinfo;
+
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        adapter=new MultiTypeAdapter();
-        Log.d("LOG","oncreat");
-        adapter.register(Voteinginfo.class,new VoteingBinder(new ICallBack() {
+        adapter = new MultiTypeAdapter();
+        Log.d("LOG", "oncreat");
+        adapter.register(Voteinginfo.class, new VoteingBinder(new ICallBack() {
             @Override
             public void OnBackResult(Object... s) {
 
             }
         }));
         vu.getRecyclerView().setAdapter(adapter);
-        Loadmore();
         adapter.setItems(list);
         vu.getRecyclerView().getAdapter().notifyDataSetChanged();
+        getvotedata(getVu().getRecyclerView().getPageManager().getCurrentIndex());
     }
+
     @Override
     public void onLoadMore(int index) {
+        getvotedata(index);
+    }
 
+
+    /**
+     * 请求数据
+     *
+     * @param currentpage
+     */
+    private void getvotedata(final int currentpage) {
+        ApiManager.getCall(ApiManager.getInstance().create(YFApi.class).getvotedata(currentpage, "1")).useCache(false).enqueue(new BaseHttpCallBack() {
+            @Override
+            public void onSuccess(ApiBean mApiBean) {
+                if (!TextUtils.isEmpty(mApiBean.getData())) {
+                    doendinfo = JSON.parseObject(mApiBean.getData(), Voteinginfolist.class);
+                    if (doendinfo != null && doendinfo.getProjectList()!=null && doendinfo.getProjectList().size()>0){
+                        if (currentpage == 0){
+                            list.clear();
+                        }
+                        list.addAll(doendinfo.getProjectList());
+                        getVu().getRecyclerView().getAdapter().notifyDataSetChanged();
+                    }else {
+                        vu.getRecyclerView().getPageManager().setPageState(PageInfo.PAGE_STATE_NO_MORE);
+                    }
+
+                }
+            }
+
+            @Override
+            public void onError(int id, Exception e) {
+
+            }
+
+            @Override
+            public void onFinish() {
+
+            }
+        });
     }
 
 
     @Override
     public void onRefresh() {
-
+        getvotedata(getVu().getRecyclerView().getPageManager().getCurrentIndex());
     }
 
     @Override
@@ -59,25 +99,4 @@ public class VoteingFragment extends BaseListFragment<VoteingContract.IView> imp
         getRootFragment().start(UIHelper.creat(allprojectsFragment.class).build());
     }
 
-    @Override
-    public void Loadmore() {
-        ApiManager.getCall(ApiManager.getInstance().create(YFApi.class).getvotedata(getVu().getRecyclerView().getPageManager().getCurrentIndex(),"1")).useCache(false).enqueue(new BaseHttpCallBack() {
-            @Override
-            public void onSuccess(ApiBean mApiBean) {
-                if (!TextUtils.isEmpty(mApiBean.getData())) {
-                    doendinfo= JSON.parseObject(mApiBean.getData(), Voteinginfolist.class);
-                    list.addAll(doendinfo.getProjectList());
-                    getVu().getRecyclerView().getAdapter().notifyDataSetChanged();
-                }
-            }
-            @Override
-            public void onError(int id, Exception e) {
-
-            }
-            @Override
-            public void onFinish() {
-
-            }
-        });
-    }
 }
